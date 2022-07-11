53b259dfccbf914c90969bcc2d8601fee8defe7f
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 20:05:39 +0300

    sukurtas projektas

---
+++ src/Programa.java
@@ -0,0 +1,24 @@
+/*
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurkite Preke.java papildomą klasę.
+3. Susikurkite Koordinate klasei kintamuosius id, pavadinimas, aprasymas, kaina, kiekis, kategorija, medziaga
+4. Susikurkite (generate...) tai klasei constructor, getter-setter, toString()
+5. Susikurkite tai klasei boolean funkciją, kuri grąžina true, jeigu prekė yra sandėlyje ir false, jeigu jos nėra.
+6. Programa.java faile susikurkite funkciją, kuri nuskaito prekių ArrayList iš failo
+7. Programa.java faile sukurkite papildomą funkciją, kuri surastų kiek iš viso prekių yra sandėlyje (susumuotų visų prekių kiekius)
+8. Programa.java faile sukurkite papildomą funkciją, kuri surastų KIEK kainuoja brangiausia prekė.
+9. Patobulinkite 8. punkto užduotį, jog grąžintų ne KIEK kainuoja brangiausia prekė, o pačios brangiausios prekės Preke objektą
+10. Preke.java faile sukurkite papildomą funkciją pritaikytiNuolaidą(), kuri bus void duomenų tipo ir per parametrus gaus kiek procentų nuolaidą padaryti prekei. Galite sakyti, jog parametras bus int procentai, jeigu darysite, kad per skliaustus gautų procentus arba iš karto galite daryti double procentai, jeigu per skliaustus norėsite, jog iš karto paduotų dešimtainę dalį. Pvz.: 15% yra tas pats kaip 0.15. Funkcija turėtų sumažinti kainą gautu procentų skaičių. Jeigu prekė prieš tai kainavo 5.99, tai atlikus funkciją ir jai padavus 20%, prekės kaina turėtų likti: 4.79
+11. Programa.java sumažinkite visų prekių, kurių kaina yra didesnė nei 600, kainas per 10%.
+12. Išveskite prekių list'o informaciją.
+13. Programa.java sumažinkite visų prekių, kurių pavadinimas yra Stalas kainas per 42.5%
+14. Išveskite prekių list'o informaciją.
+15. Programa.java faile sukurkite papildomą funkciją, kuri per parametrus gauna List<Preke> visosPrekes bei String ieskomaKategorija ir išfiltruoja iš visų prekių tik tas prekes, kurių kategorija sutampa su ieskomaKategorija. Funkcija returnina List<Preke> isfiltruotosPrekes
+16. Programa.java faile sukurkite papildomą funkciją, kuri per parametrus gauna List<Preke> prekes bei String pirmaMedziaga ir String antraMedziaga. Jūsų funkcija turėtų surasti kurios medžiagos prekių yra daugiau sistemoje. Grąžinti -1 jeigu pirmos medžiagos, 1 jeigu antros ir 0 jeigu vienodai.
+17. Patobulinkite 16. punktą, jog palygintų ne kiek skirtingų modelių yra daugiausa kažkurios medžiagos, bet pažiūrėtų kurios medžiagos prekių yra daugiausia sandėliuose (įvertinti ir jų kiekį)
+ */
+public class Programa {
+    public static void main(String[] args) {
+
+    }
+}
